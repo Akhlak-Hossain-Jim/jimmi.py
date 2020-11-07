6fe6069058640541234beb7dc28d2e4981acbd6b
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2020-11-07 13:41:59 +0600

    main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hi there whats your name?")
+name = input()
+print('hello again ' +  name)
+print("I\'m Jimmi bot and just started learning to being intelligent. So I can have some error to answering your questions.")
